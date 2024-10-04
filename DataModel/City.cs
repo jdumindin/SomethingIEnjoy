9004b60c37b9226e98b079a8f197a32a4d270496
jndumindin <jndumindin@gmail.com>
2024-10-04 15:16:40 -0700

    Add project files.

---
+++ DataModel/City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel;
+
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column("lat")]
+    public double Lat { get; set; }
+
+    [Column("lng")]
+    public double Lng { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [Column("country_id")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
